65ca070251b62d9b2b41899df67213769544e379
Garrettc96 <garrettc96@gmail.com>
2025-03-30 13:47:30 -0700

    Changing ecr command

--- publish.sh
+++ publish.sh
@@ -6,5 +6,5 @@ echo "Parsed version ${version}";
 export dockerTag="public.ecr.aws/j6e6l3c7/cacophony:${version}";
 echo "Publishing new docker tag ${dockerTag}";
 docker build . -t ${dockerTag};
-aws ecr-public get-login-password -p ecr --region us-east-1  | docker login --username AWS --password-stdin public.ecr.aws/j6e6l3c7
+aws ecr-public get-login-password --region us-east-1  | docker login --username AWS --password-stdin public.ecr.aws/j6e6l3c7
 docker push ${dockerTag};
