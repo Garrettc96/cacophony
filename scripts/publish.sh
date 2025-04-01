2f22db007e0581e7c2c23751189b8d17939f20e0
Garrettc96 <garrettc96@gmail.com>
2025-04-01 07:03:54 -0700

    test

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,6 +5,7 @@ version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 echo "Parsed version ${version}";
 export dockerTag="public.ecr.aws/j6e6l3c7/cacophony:${version}";
 echo "Publishing new docker tag ${dockerTag}";
+docker buildx create --platform linux/arm64,linux/amd64 --use
 docker buildx build --platform linux/arm64 --load . -t ${dockerTag};
 echo "${AWS_ACCESS_KEY_ID}"
 aws ecr-public get-login-password --region us-east-1  | docker login --username AWS --password-stdin public.ecr.aws/j6e6l3c7
